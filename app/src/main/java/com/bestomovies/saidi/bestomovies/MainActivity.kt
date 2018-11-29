d765d2ce933ed78673d97ddf994b8775e3fe6ea2
saidi <nourelislam.saidi@id-apps.fr>
2018-11-29 16:45:25 +0100

    WIP add retrofit singleton and API services

--- app/src/main/java/com/bestomovies/saidi/bestomovies/MainActivity.kt
+++ app/src/main/java/com/bestomovies/saidi/bestomovies/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.bestomovies.saidi.bestomovies
 
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
---
+++ app/src/main/java/com/bestomovies/saidi/bestomovies/data/TheMovieDbApiService.kt
@@ -0,0 +1,3 @@
+package com.bestomovies.saidi.bestomovies.data
+
+interface TheMovieDbApiService
